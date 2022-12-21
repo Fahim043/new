3620c0e6166aaa392c6c75e0a46c6b44feeb75a5
Fahim043 <md.fahim.afridi.ani@g.bracu.ac.bd>
2022-12-21 23:41:17 +0600

    Sprint5-470final

--- ThirdEye/ThirdEye/settings.py
+++ ThirdEye/ThirdEye/settings.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'fahimafridi043@gmail.com'
-EMAIL_HOST_PASSWORD = 'Fahim043'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'pass'
 
 KEY_ID = "rzp_test_mFPJoWCYsPNkLt"
 KEY_SECRET ="wgC3ciYeaLq43HINhUfoLIPN"
